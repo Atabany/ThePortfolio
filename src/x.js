27027a4f3e743e651bdb2efb51bd27bd62d98553
Atabany <mohamed.elatabany@leanscale.com>
2024-03-23 14:30:54 +0400

    new

---
+++ src/x.js
@@ -0,0 +1,16 @@
+
+
+
+
+import​ ​UIKit​
+​ 	
+​ 	​class​ ​CoveredClass​ {
+​ 	
+​ 	    ​static​ ​func​ ​max​(_ x: ​Int​, _ y: ​Int​) -> ​Int​ {
+​ 	        ​if​ x < y {
+​ 	            ​return​ y
+​ 	        } ​else​ {
+​ 	            ​return​ x
+​ 	        }
+​ 	    }
+​ 	}
